97891c5493854f3624afb847184d6a1e79784e1f
Michael <maestromike5@gmail.com>
2020-09-21 22:30:54 -0400

    wrote out encoder.py which seems to work. I added a quick test in main.py to make sure everything works properly. Going to start working on the UI now

--- encoder.py
+++ encoder.py
@@ -1,1 +1,24 @@
-import ffmpeg
+"""
+Author: Michael Scalzetti
+github.com/voxelvortex
+encoder.py
+
+This file runs the main bit of logic for the program to run properly
+"""
+import os
+
+
+def trim_video(in_filename, out_filename, start, end):
+    """
+    This function trims videos using ffmpeg
+
+    :param in_filename: relative path to the file that will be trimmed
+    :param out_filename: relative path to where the script will write the output
+    :param start: the start time in seconds, can include decimals
+    :param end: the end time in seconds, can include decimals
+
+    :return: None
+    """
+    command = "ffmpeg -i {0} -ss {2} -to {3} -c:v copy -c:a copy {1}".format(in_filename, out_filename, start, end)
+    os.system(command)
+
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 """
 Author: Michael Scalzetti
 github.com/voxelvortex
+main.py
 
 This file runs the main bit of logic for the program to run properly
 """
+import encoder
 
 
 def main():
-    pass
+    input_video = "video/sniper.mp4"
+    output_video = "video/sniper_trimmed.mp4"
+    encoder.trim_video(input_video, output_video, "2:55", "3:11.5")
 
 
 if __name__ =="__main__":
