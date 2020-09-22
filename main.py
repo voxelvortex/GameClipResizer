09d22b9c5a89bc8ac8fa50b93b1cca073cd6dfc1
Michael <maestromike5@gmail.com>
2020-09-21 21:12:54 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Author: Michael Scalzetti
+github.com/voxelvortex
+
+This file runs the main bit of logic for the program to run properly
+"""
+
+
+def main():
+    pass
+
+
+if __name__ =="__main__":
+    main()
